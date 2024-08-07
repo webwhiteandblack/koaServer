c650ffa4c5f90cf18daabfadc3ac19c0f31114df
jeff <>
2024-08-07 14:39:14 +0800

    update

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ import koa from "koa"
 const app = new koa();
 
 app.use((ctx)=>{
-  ctx.body = "sever2"
+  ctx.body = "sever3"
 })
 
-app.listen(3001,()=>{
+app.listen(4000,()=>{
   console.log("server2 listen on 3001")
 })
